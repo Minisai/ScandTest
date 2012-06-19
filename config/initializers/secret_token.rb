7abe513df439d4cc085f78c8e50a9bed3500104c
Anton <antonjyha@gmail.com>
2012-06-19 18:36:47 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScandTest::Application.config.secret_token = '122c7fa191cefed403d82d22c4899bcd8304e38f1e96c2efca9b47361fac81dba2fcaadfd9ad38660731b2bd2efef03e3f3ea69a6146c6271055aa5102b8dc61'
